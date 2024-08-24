8095fddf8be91f2c9db9042d98aff4be7ffd4944
Thiago183 <56850405+Thiago183@users.noreply.github.com>
2024-08-23 23:51:43 -0300

    Add files via upload
    Inserção dos arquivos

---
+++ app.js
@@ -0,0 +1,97 @@
+var textArea = document.querySelector(".text-area"); 
+var mensagem = document.querySelector('.mensagem');
+var texto = document.querySelector('.texto');
+var textoP = document.querySelector('.textoP');
+
+function btnEncriptar(){
+const textoCripto = encriptar(textArea.value);
+
+if(textoCripto.length > 0){
+    texto.innerText = "";
+    textoP.innerText = "";
+
+}
+
+
+
+mensagem.value = textoCripto;
+textArea.value = "";
+
+}
+
+
+function encriptar(mensagemEncriptada){
+    var cifraMatriz = [ 
+        ['e','enter']
+        ,['i','imes']
+        ,['a','ai']
+        ,['o','ober']
+        ,['u','ufat']];
+    mensagemEncriptada = mensagemEncriptada.toLowerCase();
+
+    const regexacentos = /[àáâãäåæçèéêëìíîïñòóôõöøùúûüýÿ]/i;
+
+    if (regexacentos.test(mensagemEncriptada)) {
+        alert("Somente caracteres sem acento");
+        return ""; 
+    }  
+
+    for(let i=0 ;i < cifraMatriz.length;i++){
+        if (mensagemEncriptada.includes(cifraMatriz[i][0]))
+            {
+            mensagemEncriptada =  mensagemEncriptada.replaceAll(cifraMatriz[i][0],cifraMatriz[i][1]);
+
+        } ; 
+
+    };
+    return mensagemEncriptada
+
+}
+
+function btnDescriptografar(){
+    const textoDescripto = descriptografar(textArea.value);
+
+    if(textoDescripto.length > 0){
+        texto.innerText = "";
+        textoP.innerText = "";
+    
+    }
+
+    mensagem.value = textoDescripto;
+    textArea.value = "";
+
+}
+
+
+function descriptografar(mensagemDescriptada){
+    var cifraMatriz = [ 
+        ['e','enter']
+        ,['i','imes']
+        ,['a','ai']
+        ,['o','ober']
+        ,['u','ufat']];
+    mensagemDescriptada = mensagemDescriptada.toLowerCase();
+        
+    
+
+    const regexacentos = /[àáâãäåæçèéêëìíîïñòóôõöøùúûüýÿ]/i;
+        
+    if (regexacentos.test(mensagemDescriptada)) {
+        alert("Somente caracteres sem acento");
+        return ""; 
+    }
+
+    for(let i=0 ;i < cifraMatriz.length;i++){
+        if (mensagemDescriptada.includes(cifraMatriz[i][0]))
+            {
+                mensagemDescriptada =  mensagemDescriptada.replaceAll(cifraMatriz[i][1],cifraMatriz[i][0]);
+
+        } ; 
+
+    };
+    return mensagemDescriptada
+
+}
+
+
+
